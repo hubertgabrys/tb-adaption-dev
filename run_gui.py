e37f8abb18d3c87cfacb09e5ed3d547bddd64631
Hubert Gabryś <hubert.gabrys@gmail.com>
2025-08-26 10:12:34 +0200

    Increase spacing between send and delete buttons

--- run_gui.py
+++ run_gui.py
@@ -421,8 +421,8 @@ def main():
         on_get_images()
 
     btn_cleanup = tk.Button(root, text="Delete selected series", command=on_cleanup)
-    btn_cleanup.grid(row=18, column=0, sticky="w", padx=10, pady=(0, 10))
-    cleanup_status.grid(row=18, column=1, sticky="w")
+    btn_cleanup.grid(row=18, column=0, sticky="w", padx=10, pady=(10, 10))
+    cleanup_status.grid(row=18, column=1, sticky="w", pady=(10, 10))
 
     # Dropdown menu for registration series
     selected_var = tk.StringVar()
